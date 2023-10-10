628272517052ab041814aff72a8eeb6e9df6276c
MinhBien <88468097+MinhBienK2@users.noreply.github.com>
2023-10-10 23:33:17 +0700

    Delete dist directory

--- dist/controllers/homeController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
-};
-const getAboutPage = (req, res) => {
-    return res.render('test/about.ejs');
-};
-module.exports = { getHomePage, getAboutPage };
